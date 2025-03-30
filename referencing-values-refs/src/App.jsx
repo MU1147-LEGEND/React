c81e026da4c9fe90924249256d9c985e3e4cf97f
Mohammad Ullah <ullahmohammad314@gmail.com>
2025-03-30 13:51:30 +0600

    Refactor App component to implement a timer with start and stop functionality; remove unused CSS file and update index styles for better layout

--- referencing-values-refs/src/App.jsx
+++ referencing-values-refs/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [start, setStart] = useState();
+    const [now, setNow] = useState(null);
+    const intervalId = useRef(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    let second = 0;
 
-export default App
+    function handleStartTimer() {
+        setNow(Date.now());
+        setStart(Date.now());
+        clearInterval(intervalId.current);
+        intervalId.current = setInterval(() => {
+            setNow(Date.now());
+        }, 100);
+    }
+
+    function handleStopTimer() {
+        if (intervalId.current != null) {
+            clearInterval(intervalId.current);
+        } else {
+            console.log("no interval running.");
+        }
+    }
+
+    if (start != null && now != null) {
+        second += (now - start) / 1000;
+    }
+
+    return (
+        <div>
+            <p>Second: {second}</p>
+            <button onClick={handleStartTimer}>Start Timer</button>
+            <button onClick={handleStopTimer}>Stop Timer</button>
+        </div>
+    );
+};
+
+export default App;
